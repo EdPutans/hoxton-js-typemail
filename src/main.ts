c64a426d3e9e4f5b9912025aec9af3843de57bdf
Ed 'Catboy' Putans <edputans@gmail.com>
2022-06-28 13:42:33 +0100

    full-template-sort-of done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-function onSubmit(e) {
-  e.preventDefault();
-  console.log('aaa')
-}
+const emails = [{
+  from: "Nico",
+  header: "Link to today's video and slides is up!",
+  content: "Link is up and you know where to find it! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+  emailAddress: 'nico@email.com',
+  img: "assets/nico.JPG"
+},
+{
+  from: "Ed",
+  header: "Congratulations! You have received a free beaver! Your name will now be displayed in the classroom's beaver list!",
+  content: "Beaver beaver beaver beaver beaver beaver beaver beaver ! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+  emailAddress: 'ed@email.com',
+  img: "assets/ed.JPG"
+},
+{
+  from: "Government",
+  header: "Time to pay your tax!",
+  content: "Pay us now! Pay us now! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.",
+  emailAddress: 'government@email.com',
+  img: "assets/gov.jpg"
+},
+  // feel free to add more emails here
+];
 
-
-function onClickEmail(email) {
-  // console.log('dab')
-}
+// write your code here! :)
